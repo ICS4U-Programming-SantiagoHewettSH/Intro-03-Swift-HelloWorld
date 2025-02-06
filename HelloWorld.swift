2826e89a6a256fd638986741d539721eb46d0886
Santiago Hewett <santiago.hewett@stu.ocsb.ca>
2025-02-06 04:38:48 +0000

    Hello world swift done

---
+++ HelloWorld.swift
@@ -0,0 +1,10 @@
+//This program greets my friend and asks them how they are
+//@author  Santiago Hewett
+//@version 1.0
+//@since   2025/02/05
+
+import Foundation  // Imports the Foundation module
+
+// Prints the message to the console
+print("Hello, Remy. ")
+print ("How are you?")
